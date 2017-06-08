a118977564ffa202dbd2938cddb66e411d7cfb90
Gregory Ditzler <ditzler@email.arizona.edu>
2017-06-08 09:06:34 -0700

    updated... now with more detail

--- keras-rnn-biodata.py
+++ keras-rnn-biodata.py
@@ -1,29 +1,38 @@
+#!/usr/bin/env python 
+'''
 
+Written by: Gregory Ditzler
+'''
 import keras
+import pickle
+import keras.backend as K
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.layers import Embedding
 from keras.layers import LSTM
-import numpy as np
 
-
-
+# -----------------------------------------------------------------------------
+# setup some free parameters of the experiment 
 data_dim = 2
 timesteps = 16
 hidden_nodes = 16
-batch_size=32
-epochs=10
-verbose=1
+batch_size = 32
+epochs = 10
+verbose = 1
 validation_split = .5
 
-'''
-(1) The first ranges 1 - 5 and denotes the motility rate: number * 0.001.
-(2) The next TWO range from 1 - 10 and denote the angiogenic rate: number * 0.05.
-(3) The last THREE range from 1 - 100 and denote the mitotic rate: number * 0.0025
-'''
-mos = [x+1 for x in range(5)]
-ans = [1,5,10]
-mis = [1,25,50,75,100]
+# -----------------------------------------------------------------------------
+# 
+# 
+# 
+# - The first ranges 1 - 5 and denotes the motility rate: number*0.001.
+# - The next TWO range from 1 - 10 and denote the angiogenic rate: number*0.05.
+# - The last THREE range from 1 - 100 and denote the mitotic rate: number*0.0025
+
+mos = [1]
+ans = [1]
+mis = [1,15,25,50,75,100]
 
 base_path = 'FLAIR_DATA/'
 num_classes = 0
@@ -32,13 +41,13 @@ for mo in mos:
         for mi in mis:
             num_classes += 1
 
-#plt.figure()
-
 curr_class = 0
 start = True
+save_name = ''
 for mo in mos:
     for an in ans:
         for mi in mis:
+            # 
             file_str = base_path+str(mo)
             if an < 10:
                 file_str += '0'
@@ -49,6 +58,7 @@ for mo in mos:
                 file_str += '0'
             file_str += str(mi) + '.txt'
             
+            #
             if start:
                 X = np.loadtxt(file_str, delimiter='\t')
                 x_train = np.zeros(( np.int(X.shape[0]/timesteps), timesteps, data_dim))  
@@ -59,6 +69,7 @@ for mo in mos:
                 start = False
 
             else:
+                # 
                 X = np.loadtxt(file_str, delimiter='\t')
                 x_t = np.zeros(( np.int(X.shape[0]/timesteps), timesteps, data_dim))  
                 y_t = np.zeros(( np.int(X.shape[0]/timesteps), num_classes))
@@ -68,35 +79,52 @@ for mo in mos:
 
                 x_train = np.concatenate( (x_train, x_t), axis = 0 )
                 y_train = np.concatenate( (y_train, y_t), axis = 0 )
-    
+
+            # 
             curr_class += 1          
 
 
-def perplexity(y_true, y_pred, mask=None):
-    if mask is not None:
-        y_pred /= keras.sum(y_pred, axis=-1, keepdims=True)
-        mask = keras.permute_dimensions(keras.reshape(mask, y_true.shape[:-1]), (0, 1, 'x'))
-        truth_mask = keras.flatten(y_true*mask).nonzero()[0]  ### How do you do this on tensorflow?
-        predictions = keras.gather(y_pred.flatten(), truth_mask)
-        return keras.pow(2, keras.mean(-keras.log2(predictions)))
-    else:
-        return keras.pow(2, keras.mean(-keras.log2(y_pred)))
-
-
+# -----------------------------------------------------------------------------
 model = Sequential()
-model.add(LSTM(hidden_nodes, return_sequences=True, input_shape=(timesteps, data_dim)))  # returns a sequence of vectors of dimension 32
-model.add(LSTM(hidden_nodes, return_sequences=True))  # returns a sequence of vectors of dimension 32
-model.add(LSTM(hidden_nodes))  # return a single vector of dimension 32
-model.add(Dense(num_classes, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy', perplexity])
+model.add(LSTM(hidden_nodes, 
+    return_sequences=True, 
+    input_shape=(timesteps, data_dim)))   
+model.add(LSTM(hidden_nodes, 
+    return_sequences=True))   
+model.add(LSTM(hidden_nodes))   
+model.add(Dense(num_classes, 
+    activation='softmax'))
+model.compile(loss='categorical_crossentropy', 
+    optimizer='adam', 
+    metrics=['accuracy'])
 
+history = model.fit(x_train, y_train, 
+    batch_size=batch_size, 
+    epochs=epochs,
+    shuffle=True, 
+    validation_data=None, 
+    validation_split=validation_split, 
+    verbose=verbose)
 
-model.fit(x_train, y_train, 
-          batch_size=batch_size,
-          epochs=epochs,
-          shuffle=True, 
-          validation_data=None, 
-          validation_split=validation_split, 
-          verbose=1)
+# -----------------------------------------------------------------------------
+# build a string that will save a file name that is specific to the task we 
+# are performing. 
+save_name = 'experiment_mo_'
+for mo in mos:
+    save_name += str(mo) + '-'
+save_name = save_name[:-1]
+save_name += '_an_'
+for an in ans:
+    save_name += str(an) + '-'
+save_name = save_name[:-1]
+save_name += '_mi_'
+for mi in mis:
+    save_name += str(mi) + '-'
+save_name = save_name[:-1]
+save_name += '_LSTM_'+str(hidden_nodes)+'_time_'+str(timesteps)+'_epoch_'+str(epochs)+'_mb_'+str(batch_size)+'.pkl'
 
+# -----------------------------------------------------------------------------
+# save the file output
+params = { 'history': history, 'model': model }
+pickle.dump(params, open(save_name, 'wb') )
 
